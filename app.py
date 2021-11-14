60dfc8fc7975a5bec023686de0a3ccf92a38f118
acidutzu <ludusnetfree@gmail.com>
2021-11-14 12:07:26 -0500

    aa

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='172.17.0.3', port=6379)
+connect_to_redis = redis.Redis(host='172.17.0.3', port=6379)
 
 
-def counter():
-    retries = 5
+def za_count():
+    retry = 5
     while True:
         try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
+            return connect_to_redis.incr('tickings')
+        except redis.exceptions.ConnectionError as errcon:
+            if retry == 0:
+                raise errcon
+            retry -= 1
             time.sleep(0.5)
 
 
 @app.route('/')
 def hihou():
-    count = counter()
-    return 'Salutare si bine v-am gasit dragii mei,cineva spera ca am ramas fara idei... | mesajul a fost vazut de {} ori.\n'.format(count)
+    counting_page = za_count()
+    return 'Salutare si bine v-am gasit dragii mei,cineva spera ca am ramas fara idei... | mesajul a fost vazut de {} ori.\n'.format(counting_page)
 
 
 if __name__ == "__main__":
